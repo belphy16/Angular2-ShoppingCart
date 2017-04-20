9bf63b249a6c0af3f476c1cac220da69ddbce669
Satish Kumar Kamavaram <satishkamavaram@gmail.com>
2017-04-20 07:50:33 +0530

    added catalog

--- src/app/catalog/catalog.service.ts
+++ src/app/catalog/catalog.service.ts
@@ -12,7 +12,7 @@ export class CatalogService
  catalogSelected = new EventEmitter<Catalog>();
  catalogChanged   = new Subject<Catalog[]>();
 
-private  catalogs: Catalog[] = [
+private  catalogs2: Catalog[] = [
     new Catalog("catalog1","catalogDesc1",
     "https://firebearstudio.com/blog/wp-content/uploads/2016/01/Best-Node.JS-Books-1024x551.jpg",
     [new ShoppingItem('book1','10'),
@@ -23,6 +23,32 @@ private  catalogs: Catalog[] = [
     [new ShoppingItem('book3','30'),
     new ShoppingItem('book4','40')])
   ];
+
+  private  catalogs: Catalog[] = [
+      new Catalog("NodeJS","ServerSide JavaScript Event Driven Programming",
+      "https://firebearstudio.com/blog/wp-content/uploads/2016/01/Best-Node.JS-Books-1024x551.jpg",
+      [new ShoppingItem('book1','10'),
+      new ShoppingItem('book2','20'),
+    new ShoppingItem('book3','30')]),
+
+   new Catalog("MongoDB","Document Oriented NOSQL DB",
+      "https://www.zoomdata.com/sites/default/files/styles/712_width/public/MongoDB_Logo_cropped.png?itok=jai5MXZh",
+      [new ShoppingItem('book1','10'),
+      new ShoppingItem('book2','20'),
+    new ShoppingItem('book3','30')]),
+
+   new Catalog("Angular 4","Google - JavaScript Frontend Framework",
+  "https://cdn-images-1.medium.com/max/796/1*juPyda3wq9uz_SNFRLuANg@2x.png",
+  [new ShoppingItem('book1','10'),
+      new ShoppingItem('book2','20'),
+    new ShoppingItem('book3','30')]),
+
+   new Catalog("FullStack","Angular , NodeJs , MongoDB",
+  "https://media.licdn.com/mpr/mpr/shrinknp_400_400/AAEAAQAAAAAAAARoAAAAJGFjZTcxNzJlLTI4MTAtNGM4NS04MjZhLTViYjEwZTU3YTA2ZA.png",
+  [new ShoppingItem('book3','30'),
+      new ShoppingItem('book4','40')])
+    ];
+
 
   private  catalogs1 : Catalog[] = [
 
